5dccfdd092903a2be52417ebc8326c611aab3646
MarcGroc <marcingrochola1@gmail.com>
2023-02-21 17:27:30 +0100

    port error fix

--- recruitment_task/settings.py
+++ recruitment_task/settings.py
@@ -71,7 +71,20 @@ WSGI_APPLICATION = 'recruitment_task.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "pytania",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
+    }
+
+else:
+    DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
             "NAME": os.environ.get("POSTGRES_DB"),
